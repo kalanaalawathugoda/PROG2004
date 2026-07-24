88d70814a8d0ac6c90d45cb338fc6c90b4463ce1
kalanaalawathugoda <kalanaalwathugoda@gmail.com>
2026-07-25 02:06:33 +0530

    Create initial Java project and AssignmentOne class

---
+++ src/AssignmentOne.java
@@ -0,0 +1,8 @@
+public class AssignmentOne {
+
+    public static void main(String[] args) {
+        System.out.println("PROG2004 Assignment 1");
+        System.out.println("Health Service Appointment System");
+        System.out.println("System started successfully");
+    }
+}
